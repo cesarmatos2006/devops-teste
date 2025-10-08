2399eeef577fbe498d1614516d893ee92cb8c202
lab05l <matoscesar36@gmail.com>
2025-10-07 22:00:05 -0300

    ajuste na calculadora e testes

--- tests/test_calculadora.py
+++ tests/test_calculadora.py
@@ -1,5 +1,10 @@
-from src.calculadora import soma
+from src.calculadora import soma, subtracao, multiplicacao
 
 def test_soma():
   assert soma(2, 3) == 5
 
+def test_subtracao():
+  assert subtracao(5, 2) == 3
+
+def test_multiplicacao():
+  assert multiplicacao(5, 2) == 10
